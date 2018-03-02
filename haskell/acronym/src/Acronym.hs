80a6e157c77bb0f3cb3a4c9617e1555a08d41f00
Vadim Manaenko <vadim.razorq@gmail.com>
2018-03-02 13:57:12 +0300

    add hs/acronym

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -1,4 +1,16 @@
 module Acronym (abbreviate) where
 
+import Data.List.Split
+import Data.Char
+
+getLetters :: String -> String
+getLetters w = if check then [first] else first : filtered
+    where
+        first = toUpper $ head w
+        filtered = filter isUpper (tail w)
+        check = length (tail w) == length filtered
+
 abbreviate :: String -> String
-abbreviate xs = error "You need to implement this function."
+abbreviate xs = 
+    let ws = filter (not . null) $ splitOneOf " ,-" xs
+    in concat $ map getLetters ws
