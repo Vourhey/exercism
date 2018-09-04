265e19a06bd991889b2fae05af406e72c6366e72
Vadim Manaenko <vadim.razorq@gmail.com>
2018-09-04 12:52:05 +0300

    haskell/prime-factors

--- haskell/prime-factors/src/PrimeFactors.hs
+++ haskell/prime-factors/src/PrimeFactors.hs
@@ -1,4 +1,12 @@
 module PrimeFactors (primeFactors) where
 
+recursiveChecks :: Integer -> Integer -> [Integer]
+recursiveChecks 1 _ = []
+recursiveChecks n d = 
+    if n `mod` d == 0 then 
+        d : recursiveChecks (n `div` d) d 
+    else 
+        recursiveChecks n (d + 1)
+
 primeFactors :: Integer -> [Integer]
-primeFactors n = error "You need to implement this function."
+primeFactors n = recursiveChecks n 2
